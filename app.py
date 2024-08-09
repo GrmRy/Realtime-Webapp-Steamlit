0a6566aa000e2f64d93551e180da088f1a891965
Andrea Satria Nagari <93112763+GrmRy@users.noreply.github.com>
2024-08-09 15:44:44 +0000

    update

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoHTMLAttributes
 import numpy as np
 import av
+#to install libgl in codespaces : sudo apt update -> sudo apt install libgl1-mesa-glx 
+st.title("OpenCV Filters on Video Stream")
 
-st.title("Open CV Filters on Streamlit Webcam")
 filter = "none"
-#to install libgl in codespaces : sudo apt update -> sudo install libgl1-mesa-glx 
+
+
 def transform(frame: av.VideoFrame):
     img = frame.to_ndarray(format="bgr24")
 
@@ -15,6 +17,8 @@ def transform(frame: av.VideoFrame):
     elif filter == "canny":
         img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
     elif filter == "grayscale":
+        # We convert the image twice because the first conversion returns a 2D array.
+        # the second conversion turns it back to a 3D array.
         img = cv2.cvtColor(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
     elif filter == "sepia":
         kernel = np.array(
@@ -27,6 +31,7 @@ def transform(frame: av.VideoFrame):
         pass
 
     return av.VideoFrame.from_ndarray(img, format="bgr24")
+
 
 col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1, 1, 1])
 
@@ -50,5 +55,8 @@ with col6:
         filter = "invert"
 
 
-webrtc_streamer(key="streamer",video_frame_callback=transform ,sendback_audio=False)
-
+webrtc_streamer(
+    key="streamer",
+    video_frame_callback=transform,
+    sendback_audio=False
+    )
